0fd5ca181920762c1d3036a3870fe3dcfbc73473
Erikson Ferreira <erikson@densyx.com.br>
2023-02-19 19:48:07 -0300

    user models

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+"""
+Database Models
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    """
+    Manager for users
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create, save and return a new user.
+        Args:
+            email (str): User email
+            password (str, optional): User password. Defaults to None.
+        """
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    User in system
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'

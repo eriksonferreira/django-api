efb61d0b2fd53f2fc7133fdd09549207e2d2470d
Erikson Ferreira <erikson@densyx.com.br>
2023-02-18 21:41:17 -0300

    added tests, db config and psycopg2 install

---
+++ app/app/calc.py
@@ -0,0 +1,20 @@
+"""
+Calculator
+"""
+
+
+def add(x, y):
+    '''
+    Add x and y and return result
+    '''
+    return x + y
+
+
+def subtract(x, y):
+    """subtract x and y
+
+    Args:
+        x (float): any number
+        y (foat): number
+    """
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,24 @@
+"""
+Sample test
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module
+
+    Args:
+        SimpleTestCase (float): Test adding numbers
+    """
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract(self):
+        """subtract numbers
+        """
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, -5)
